b53cfa9d919feee24467a3d145b8d6b81eefa541
miyaluc <srlucas3@gmail.com>
2017-10-18 11:29:26 -0700

    migrations added

---
+++ lab27_miya/lab27_miya/Migrations/20171018182725_Initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace lab27_miya.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CPS",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    HasCompassion = table.Column<bool>(type: "bit", nullable: false),
+                    Region = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    YearsInService = table.Column<int>(type: "int", nullable: false),
+                    Zodiac = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CPS", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CPS");
+        }
+    }
+}
