5cd154a03ca9d61029556c8cb809adf7b7ab2742
miyaluc <srlucas3@gmail.com>
2017-10-18 11:06:00 -0700

    added CPS class & Home controller, added middleware to Startup

---
+++ lab27_miya/lab27_miya/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab27_miya.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private lab27_miyaContext _context;
+
+        //public HomeController(lab26_miyaContext context)
+        //{
+        //    _context = context;
+        //}
+
+        public IActionResult Index()
+        {
+            //var result = _context.CPS.Where(c => c.ID == 1);
+            //result.ToList() - put this is View
+
+            return View();
+        }
+    }
+}
+
