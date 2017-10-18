4c8b3f03c935b793da66e8e6693a858a21c43143
miyaluc <srlucas3@gmail.com>
2017-10-18 11:11:35 -0700

    added application user class

---
+++ lab27_miya/lab27_miya/Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab27_miya.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Circumstances
+        {
+            get; set;
+        }
+    }
+}
